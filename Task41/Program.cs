7fd67a161795c21ec7536751de22051132500ad5
Maria Yutaeva <mariya1982@yandex.ru>
2022-10-05 22:02:01 +0300

    задача41

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -2,22 +2,30 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
-void FillArray(int[] arr)
-{
-    for (int i = 0; i < arr.Length; i++)
-    {
-        Console.Write("Введите любые числа (через пробел): ");
-        arr[i] = Convert.ToInt32(Console.ReadLine());
-    }
-}
 
-void PrintArray(int[] arr)
+Console.WriteLine("Введите количество чисел в массиве: ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] massive = new double[size];
+FillArray(massive);
+CountPositive(massive);
+
+
+void FillArray(double[] arr)
 {
     for (int i = 0; i < arr.Length; i++)
     {
-        Console.Write(arr[i] + " ");
+        Console.Write($"Введите число {i+1}: ");
+        arr[i] = Convert.ToDouble(Console.ReadLine());
     }
 }
-int[] massiv = new int[5];
-FillArray(massiv);
-PrintArray(massiv);
+void CountPositive(double[] arr)
+{
+int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+        count++;
+   
+}
+Console.WriteLine($"Кол-во элементов больше 0: {count}");
+}
